814b9ec77df630c233c74f0e1028a89fd5463141
Gabe Rundlett <gabe@rundlett.com>
2019-10-18 15:20:23 -0400

    updated engine

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,13 +1,30 @@
 #include <coel.hpp>
+#include <iostream>
+#include <math.hpp>
+
+namespace test {
+    struct Window : coel::Window {
+        math::dVec2 mouse = {0, 0};
+
+        Window(const unsigned int w, const unsigned int h, const char *const t) : coel::Window(w, h, t) {
+            coel::renderer::batch2d::init(w, h);
+        }
+
+        void mouse_move(const coel::MouseMove &e) override { //
+            mouse = {e.x, e.y};
+        }
+
+        void on_update() override {
+            coel::renderer::clear(0.5f);
+            coel::renderer::batch2d::submit_rect(mouse.x, mouse.y, 100, 100);
+            coel::renderer::batch2d::flush();
+        }
+    };
+} // namespace test
 
 int main() {
-    coel::Window window(1600, 900, "title");
-    coel::gui::init();
-
+    test::Window window(1600, 800, "title");
     while (!window.should_close()) {
-        coel::renderer::clear(0.5f);
-        coel::gui::draw_rect(0, 0, 1, 1);
-        coel::gui::flush();
         window.update();
     }
 }
