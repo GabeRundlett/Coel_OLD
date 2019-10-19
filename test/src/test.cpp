a92ed7c0b59c5eefb517d5daf855f7d847851b0a
GabeRundlett <gabe@rundlett.com>
2019-10-18 17:14:02 -0700

    Updated renderer (NOT WORKING)

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -3,11 +3,14 @@
 #include <math.hpp>
 
 namespace test {
+    static constexpr math::Color col = {255, 0, 255, 255};
+
     struct Window : coel::Window {
         math::dVec2 mouse = {0, 0};
 
         Window(const unsigned int w, const unsigned int h, const char *const t) : coel::Window(w, h, t) {
             coel::renderer::batch2d::init(w, h);
+            coel::renderer::batch2d::fill_color(col);
         }
 
         void mouse_move(const coel::MouseMove &e) override { //
@@ -15,8 +18,7 @@ namespace test {
         }
 
         void on_update() override {
-            coel::renderer::clear(0.5f);
-            coel::renderer::batch2d::submit_rect(mouse.x, mouse.y, 100, 100);
+            coel::renderer::batch2d::fill_rect(mouse.x, mouse.y, 100, 100);
             coel::renderer::batch2d::flush();
         }
     };
