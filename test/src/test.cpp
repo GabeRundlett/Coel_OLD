4a2f4ce1f2b1dadec66b2d0ee819cb8bf20a3492
GabeRundlett <gabe@rundlett.com>
2019-11-14 09:20:53 -0500

    Updated math library

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,36 +1,19 @@
 #include <coel.hpp>
 
+math::Vec2 vertices[] = {-0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5};
+unsigned short indices[] = {0, 1, 2, 1, 3, 2};
 class MyApp final : public coel::App {
-    coel::Ref<coel::Window> m_window;
+    coel::Ref<coel::Vbo> vbo = coel::create_vbo(vertices, sizeof(vertices), {{coel::Type::f32, 2}});
+    coel::Ref<coel::Ibo> ibo = coel::create_ibo(indices, sizeof(indices));
+    coel::Ref<coel::Vao> vao = coel::create_vao();
 
   public:
-    MyApp() {}
+    MyApp() { vao->add_vbo(vbo); }
+    void update() override {
+        vao->draw(ibo); //
+    }
 };
 
 coel::Scope<coel::App> coel::create_app() { return coel::create_scope<MyApp>(); }
-
-/*
-int main() {
-    auto window = coel::create_window(800, 800, "Title");
-    window->on_key_press([](const coel::KeyPress &e) {
-        std::cout << "Key Pressed!\n"; //
-    });
-
-    float vertices[] = {-0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5};
-    unsigned short indices[] = {0, 1, 2, 1, 3, 2};
-    auto vbo = coel::create_vbo(vertices, sizeof(vertices), {{coel::Type::f32, 2}});
-    auto ibo = coel::create_ibo(indices, sizeof(indices));
-    auto vao = coel::create_vao();
-    vao->add_vbo(vbo);
-
-    double prev_time = window->get_time();
-    size_t fps = 0;
-
-    while (window->is_open()) {
-        vao->draw(ibo);
-        window->update();
-    }
-}
-*/
 
 // hi honey i love you !
