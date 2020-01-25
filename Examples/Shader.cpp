57888fe0a527f7ca9fa39e86a23cec4ad8de2a49
GabeRundlett <gabe@rundlett.com>
2020-01-25 14:58:21 -0500

    WIP

--- Examples/Shader.cpp
+++ Examples/Shader.cpp
@@ -9,7 +9,7 @@ int main() {
     layout (location = 0) in vec2 pos;
 
     void main() {
-	    gl_Position = vec4(pos, 0, 1);
+	    gl_Position = vec4(pos / 2, 0, 1);
     })",
                       *const fragSrc = R"(
     #version 440
@@ -24,11 +24,12 @@ int main() {
         Math::Vec2 pos;
     };
     Vertex vertices[] = {
-        {-0.5, -0.5},
-        {0.5, -0.5},
-        {0, 0.5},
+        {-1, -1},
+        {1, -1},
+        {-1, 1},
+        {1, 1},
     };
-    unsigned short indices[] = {0, 1, 2};
+    unsigned short indices[] = {0, 1, 2, 1, 3, 2};
 
     auto vbo = Coel::createVbo(vertices, sizeof(vertices), {{Coel::Type::f32, 2}});
     auto ibo = Coel::createIbo(indices, sizeof(indices));
